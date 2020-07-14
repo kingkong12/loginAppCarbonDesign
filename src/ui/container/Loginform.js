e00a18a64fd15bc8ae360c3f6a49d671c5849987
Mithil <vasanimithil999@gmail.com>
2020-07-14 23:49:06 +0530

    multi step for created

--- src/ui/container/Loginform.js
+++ src/ui/container/Loginform.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import {
   Link,
   FluidForm,
@@ -9,12 +9,85 @@ import {
 } from 'carbon-components-react'
 import styled from 'styled-components'
 // constants-helpers
-import organizationMinLength from '../../const/fieldConstants'
+import { organizationMinLength, emailRegex } from '../../const/fieldConstants'
 import query from '../../const/mediaQuery'
+import errorMessages from '../../const/errorMessages'
 // components
 import { PageHeader, SubHeader } from './Regsitration'
 
 const Loginform = () => {
+  const emailRef = useRef(null)
+  const [state, setState] = useState({
+    loginEmail: '',
+    loginPassword: '',
+    emailError: '',
+    passwordError: '',
+    step: 1
+  })
+  const { loginEmail, loginPassword, emailError, step, passwordError } = state
+  const emailValidation = (value) => {
+    if (value < 6) {
+      setState({ ...state, emailError: errorMessages.lessthanSix })
+    } else if (!emailRegex.test(String(value).toLowerCase())) {
+      setState({ ...state, emailError: errorMessages.vaildEmail })
+    } else {
+      setState({ ...state, emailError: '' })
+    }
+  }
+
+  const renderForm = () => {
+    switch (step) {
+      case 1:
+        return (
+          <TextInput
+            ref={emailRef}
+            value={loginEmail}
+            onInput={(e) => setState({ ...state, loginEmail: e.target.value })}
+            type="text"
+            id="loginEmail"
+            labelText="Email"
+            placeholder="john.doe@example.com"
+            invalid={Boolean(emailError)}
+            required
+            invalidText={emailError}
+            maxLength={organizationMinLength}
+            onBlur={(e) => emailValidation(e.target.value)}
+          />
+        )
+      case 2:
+        return (
+          <TextInput
+            //ref={emailRef}
+            value={loginPassword}
+            onInput={(e) =>
+              setState({ ...state, loginPassword: e.target.value })
+            }
+            type="passowrd"
+            id="password"
+            labelText="Password"
+            placeholder="****"
+            invalid={Boolean(passwordError)}
+            required
+            invalidText={passwordError}
+            maxLength={organizationMinLength}
+            //onBlur={(e) => emailValidation(e.target.value)}
+          />
+        )
+      default:
+        return <div> Failed to load form </div>
+    }
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    // const formValue = document.getElementById('loginEmail').value
+    // console.log('faornvalue')
+    if (state.emailError) {
+      return
+    }
+    if (state.step === 1) setState({ ...state, step: 2 })
+  }
+
   return (
     <Container>
       <PageHeader>Log in</PageHeader>
@@ -22,23 +95,17 @@ const Loginform = () => {
         Don't have an Account? <Link> Register Now </Link>
       </SubHeader>
       <StyledFormLabel> Enter Your Strobes ID</StyledFormLabel>
-      <FluidForm>
-        <TextInput
-          type="text"
-          id="email"
-          defaultValue=""
-          labelText="Email"
-          placeholder="john.doe@example.com"
-          invalid={false}
-          invalidText={'Invalid text'}
-          maxLength={organizationMinLength}
-        />
+      <FluidForm id="loginform" onSubmit={(e) => onSubmit(e)}>
+        {renderForm()}
         <Button type="submit"> Continue </Button>
       </FluidForm>
       <ForgotPassword>
         <Checkbox id="remember-me" labelText="Remember Me" />
         <Link> Forgot you Passowrd ? </Link>
       </ForgotPassword>
+      <AlternateLogin>
+        <Link> Alternate Login </Link>
+      </AlternateLogin>
     </Container>
   )
 }
@@ -65,6 +132,10 @@ const Button = styled(CarbonButton)`
 const ForgotPassword = styled.div`
   display: flex;
   justify-content: flex-end;
+`
+
+const AlternateLogin = styled.div`
+  margin-top: 37px;
 `
 
 export default Loginform
